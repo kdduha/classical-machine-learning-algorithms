73de919288b61a3b70b27b5a5d97d618c5f2ce31
Andrey Kuznetsov <113019270+kdduha@users.noreply.github.com>
2023-09-17 14:23:58 +0300

    metric fixes

--- linear_models/LogisticRegression.py
+++ linear_models/LogisticRegression.py
@@ -2,15 +2,16 @@ import pandas as pd
 import numpy as np
 from typing import Optional, Union
 import random
+from sklearn.metrics import roc_auc_score
 
 
 class MyLogReg():
     """
-    LinearRegression
+    Binary LogisticRegression
     
     Parametres:
     n_iter = the number of iterations during training
-    learing_rate = the step of training
+    learning_rate = the step of training
     reg = the type of gradiant regularization (None, l1, l2, elasticnet)
     l1_coef = the coef for the Lasso (l1) and ElasticNet
     l2_coef = the coef for the Rigde (l2) and ElasticNet
@@ -65,10 +66,12 @@ class MyLogReg():
             
             # new iter prediction
             pred_y = X_batch @ self.__weights
-            proba = self._sigmoid(pred_y)
+            
+            # sigmoid values
+            proba_y = self._sigmoid(pred_y)
           
             # gradient based on the type of regularization
-            grad = grad = 1/X_batch.shape[0] * (proba - y.T) @ X_batch
+            grad = self.__gradient(X_batch, y_batch, pred_y, proba_y, self.__reg, self.__l1, self.__l2)
           
             # minimization step (could be dynamic)
             k = self.__learning_rate if type(self.__learning_rate) in (int, float) else self.__learning_rate(i+1)
@@ -77,38 +80,36 @@ class MyLogReg():
             self.__weights -= k * grad
             
             # logging loss func each 'verbose' step
-            self.__logging_loss(i, verbose, y, pred_y, proba)
+            self.__logging_loss(i, verbose, y, pred_y, proba_y)
 
-  
-     def _sigmoid(self, y):
+    # sigmoid func
+    def _sigmoid(self, y: np.ndarray) -> np.ndarray:
         return 1 / (1 + np.exp(-y))
-        
         
     # getting coefs without w_0
     def get_coef(self) -> np.ndarray:
         return self.__weights[:-1]
 
-
-    # getting prediction
+    # getting classes' prediction
     def predict(self, X: pd.DataFrame) -> np.ndarray:
         return self.predict_proba(X) > 0.5
 
-
-    # getting prediction
+    # getting proba prediction
     def predict_proba(self, X: pd.DataFrame) -> np.ndarray:
         # adding ones in the end of X matrix for the w_0
         X = np.c_[X.values, np.ones(X.shape[0])]
         return self._sigmoid(X @ self.__weights)
 
-    
     # getting the score by the chosen metric on the last iter step
     def get_best_score(self):
-        # if X and Y are not None
-        return self.__metric_func(self.predict(self.X), self.y)
+        # works if X and Y are not None
+        if self.__metric == 'roc_auc':
+            return self.__metric_func(self.y, self.predict_proba(self.X))
+        return self.__metric_func(self.y, self.predict(self.X))
     
     
-
-   def _confusion_matrix(self, y, pred_y):
+    # confusion matrix like in sklearn
+    def _confusion_matrix(self, y: np.ndarray, pred_y: np.ndarray) -> np.ndarray:
         # [[TN, FN], [FP, TP]]
         # initialize the confusion matrix
         classes = np.unique(y)
@@ -119,50 +120,67 @@ class MyLogReg():
                 # count the number of instances in each combination of actual / predicted classes
                 conf_matrix[i, j] = np.sum((y == classes[i]) & (pred_y == classes[j]))
               
-        return cconf_matrix
+        return conf_matrix
+
+    # deffault loss func
+    def _loss(self, y: np.ndarray, proba_y: np.ndarray) -> float:
+        return np.mean(y * np.log(proba_y + 1e-15) + (1 - y) * np.log(1 - proba_y + 1e-15))
     
-    def _accuracy(self, y, pred_y):
+    # computing accuracy 
+    def _accuracy(self, y: np.ndarray, pred_y: np.ndarray) -> float:
+        y = self.__check_y_dimension(y) 
         conf_matrix = self._confusion_matrix(y, pred_y)
         return (conf_matrix[0, 0] + conf_matrix[1, 1]) / np.sum(conf_matrix)
     
-    
-    def _precision(self, y, pred_y):
+    # computing precision
+    def _precision(self, y: np.ndarray, pred_y: np.ndarray) -> float:
+        y = self.__check_y_dimension(y) 
         conf_matrix = self._confusion_matrix(pred_y, y)
         return conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[1, 0])
 
-    def _recall(self, y, pred_y):
+    # computing recall
+    def _recall(self, y: np.ndarray, pred_y: np.ndarray) -> float:
+        y = self.__check_y_dimension(y) 
         conf_matrix = self._confusion_matrix(pred_y, y)
         return conf_matrix[1, 1] / (conf_matrix[1, 1] + conf_matrix[0, 1])
 
-    def _f1(self, y, pred_y):
+    # computing F1
+    def _f1(self, y: np.ndarray, pred_y: np.ndarray) -> float:
+        y = self.__check_y_dimension(y) 
         conf_matrix = self._confusion_matrix(pred_y, y)
         prec = self._precision(y, pred_y)
         rec = self._recall(y, pred_y)
         return 2 * (prec * rec) / (prec + rec)
 
-    def _rocauc(self, y, y_proba):
-        y_proba = np.sort(y_proba)[::-1]
-        pred_y = y_proba > 0.5
+    # computing ROC AUC
+    def _roc_auc(self, y: np.ndarray, proba_y: np.ndarray) -> float:
+        
+        y, proba_y = pd.DataFrame(y, columns=[1]), pd.DataFrame(proba_y)
+        df = pd.concat([np.round(proba_y, 10), y], axis=1).sort_values(by=0, ascending=False)
 
-        score_sum = []
-        for i in range(1, len(pred_y)):
-            positive_classes = np.sum(pred_y[:i])
-            same_score = np.sum(pred_y[:i] == y_proba[i])/2
-            score_sum.append(positive_classes + same_score)
-        return sum(score_sum)
+        positives = df[df[1] == 1]
+        negatives = df[df[1] == 0]
+
+        total = 0
+        for score in negatives[0]:
+            positive_score = (positives[0] > score).sum()
+            same_score = (positives[0] == score).sum()
+            total += positive_score + 0.5 * same_score
+
+        return total / (positives.shape[0] * negatives.shape[0])
     
     # logging loss func on the chosen iter step
-    def __logging_loss(self, i: int, verbose: int, y: np.ndarray, pred_y: np.ndarray, y_proba) -> None:
+    def __logging_loss(self, i: int, verbose: int, y: np.ndarray, pred_y: np.ndarray, y_proba: np.ndarray) -> None:
         # if this is a necessary step
         if verbose and (i+1) % verbose == 0:
-                if self.__metric == 'rocauc':
+                if self.__metric in ('roc_auc', 'LogLoss', None):
                     error = self.__metric_func(y, proba_y)
                 else:
                     error = self.__metric_func(y, pred_y)
                 print(f"{i if not i else 'start'} | {self.__metric} loss: {error}")
     
     # computing gradient
-    def __gradient(self, X: np.ndarray, y: np.ndarray, pred_y: np.ndarray, 
+    def __gradient(self, X: np.ndarray, y: np.ndarray, pred_y: np.ndarray, proba_y: np.ndarray,  
                    reg: Optional[int], l1: Union[int, float], l2: Union[int, float]) -> np.ndarray:
          
         # checking regularization type
@@ -171,7 +189,7 @@ class MyLogReg():
             return f'There is no such regularization. You can choose from {reg_types}'
         
         # computing deffault gradient
-        grad = 2/X.shape[0] * (pred_y - y.T) @ X
+        grad = 1/X.shape[0] * (proba_y - y.T) @ X
         if reg is reg_types[0]:
             return grad
         # lasso MAE
@@ -183,23 +201,20 @@ class MyLogReg():
         # elasticnet
         return grad + l1 * np.sign(self.__weights) + l2 * 2 * self.__weights
 
-
-
-
     # checking given metrics and returning the metric function
     def __check_metrics(self, metric: Optional[str]):
-        # ACCURACY by deffault
+        # LogLoss by deffault
         if metric is None:
-            return 'accuracy', self._accuracy
+            return 'LogLoss', self._loss
 
-        dict_metrics = {'accuracy': self._accuracy, 'precision': self._precision, 
-                        'recall': self._recall, 'f1': self._f1, 'rocauc': self._rocauc}
+        dict_metrics = {'LogLoss': self._loss, 'accuracy': self._accuracy, 'precision': self._precision, 
+                        'recall': self._recall, 'f1': self._f1, 'roc_auc': self._rocauc}
         try:
             return metric, dict_metrics[f'{metric}']
         # if there is no such metric choose the default one
         except KeyError:
             f'Sorry, there is no such metric. You can choose from {dict_metrics.values}'
-            return 'accuracy', self._accuracy
+            return 'LogLoss', self._loss
     
     # checking Y dimension for being (n,) not (1, n)
     def __check_y_dimension(self, y) -> bool:
